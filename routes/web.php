b610e3db1277fd6601a0a7c4e96f0d01d8064675
phamngocpho <phamngocpho1606@gmail.com>
2024-11-07 15:59:04 +0700

    new commit

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', [App\Http\Controllers\AuthController::class, 'showLoginForm'])->name('login');
