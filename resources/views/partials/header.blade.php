811f937929ceac2955483ab61c3eca3f07e56014
phamngocpho <phamngocpho1606@gmail.com>
2024-11-07 21:41:59 +0700

    commit 04

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,15 @@
+<header>
+    <nav class="container mx-auto flex justify-between items-center py-4">
+        <a href="/" class="text-2xl font-bold">Traveling</a>
+        <ul class="flex space-x-6">
+            <li><a href="/holiday-packages">Holiday Packages</a></li>
+            <li><a href="/top-deals">Top Deals</a></li>
+            <li><a href="/help">Help</a></li>
+            <li><a href="/wishlist">Wishlist</a></li>
+        </ul>
+        <div class="flex space-x-4">
+            <a href="/register" class="py-2 px-4">Register</a>
+            <a href="/login" class="bg-blue-400 text-white py-2 px-4 rounded">Login</a>
+        </div>
+    </nav>
+</header>
